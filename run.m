111a457aee52c1afc245e5a86c6f034074ca2ff9
maohhgg <maohhgg@gmail.com>
2021-03-16 09:35:19 +0800

    clear

--- run.m
+++ run.m
@@ -2,15 +2,15 @@ clear; clc;
 addpath(genpath(pwd)); 
 
 %%% configuration.
-RM.AStar                    = 0.85;                                                  % pre-specified system availability requirement
-RM.interval                 = [4 6 10 18 30 50 80 120];                              % Replacement interval
-RM.maxReplacementInterval   = length(RM.interval);                                   % maximal number of replacement interval choices              
-RM.replacementTime          = 0.001*[6 8 7 8 6 8 9 11 12 11];                        % replacement time
-RM.systemLifetime           = 120;                                                   % System lifetime 
-RM.replacementCost          = [24 29 37 44 49 55 58 64 71 76];                       % replacement cost
-RM.repairCost               = [2.9 2.3 2.5 3.2 3.2 2.7 2.5 3.1 3.2 2.1];             % minimal repair cost
-RM.repairTime               = [0.38 0.37 0.31 0.32 0.29 0.31 0.37 0.41 0.39 0.37];   % minimal repair time
-RM.penalty                  = 9999999;
+RM.AStar					= 0.85;                                                  % pre-specified system availability requirement
+RM.interval					= [4 6 10 18 30 50 80 120];                              % Replacement interval
+RM.maxReplacementInterval	= length(RM.interval);                                   % maximal number of replacement interval choices              
+RM.replacementTime			= 0.001*[6 8 7 8 6 8 9 11 12 11];                        % replacement time
+RM.systemLifetime			= 120;                                                   % System lifetime 
+RM.replacementCost			= [24 29 37 44 49 55 58 64 71 76];                       % replacement cost
+RM.repairCost				= [2.9 2.3 2.5 3.2 3.2 2.7 2.5 3.1 3.2 2.1];             % minimal repair cost
+RM.repairTime				= [0.38 0.37 0.31 0.32 0.29 0.31 0.37 0.41 0.39 0.37];   % minimal repair time
+RM.penalty					= 9999999;
 
 % the expected failure for each elment for each replacement interval
 RM.lamda = [0.5 1.2 3.2 6.2 18.1 42.2 94.7 165.5;
@@ -25,28 +25,28 @@ RM.lamda = [0.5 1.2 3.2 6.2 18.1 42.2 94.7 165.5;
        0.6	1.4	3.1	6.5	17.9 45.4 97.1 174.3];   
    
 %%% system configuration.
-SWS.k       = 2;   
-SWS.r       = 4;
-SWS.m       = 10;  
-SWS.W       = [0.2 40; 0.8 45];     
-SWS.type    = 'even';        % system type  even/uneven
+SWS.k		= 2;   
+SWS.r		= 4;
+SWS.m		= 10;  
+SWS.W		= [0.2 40; 0.8 45];     
+SWS.type	= 'even';        % system type  even/uneven
 
 %%% genetic algorithm configuration. (GA)
-GA.populationSize   = 25;             % population size
-GA.maxIteration     = 450;            % maximal number of iteration
-GA.crossoverRate    = 0.8;
-GA.mutationRate     = 0.2;
+GA.populationSize	= 25;             % population size
+GA.maxIteration		= 450;            % maximal number of iteration
+GA.crossoverRate	= 0.8;
+GA.mutationRate		= 0.2;
 
 Config.GA 			= GA;
 Algorithm.GA		= @GeneticAlgorithm;
-
+claer GA;
 
 
 names = fieldnames(Config);
 
 for i = 1:length(names)
 	field = string(names(i));
-    [Excel.(field), Images.(field)] = Algorithm.(field)(SWS, RM, Config.(field));
+	[Excel.(field), Images.(field)] = Algorithm.(field)(SWS, RM, Config.(field));
 end
 
 SaveExcel(Excel, SWS, RM)
